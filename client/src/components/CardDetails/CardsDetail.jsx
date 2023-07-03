932951218a6b5e5ffc75c0c136f4d022df6acec0
Ariel-Alegre <arielalegre98@gmail.com>
2023-07-03 18:16:52 -0300

    json install

--- client/src/components/CardDetails/CardsDetail.jsx
+++ client/src/components/CardDetails/CardsDetail.jsx
@@ -71,7 +71,7 @@ export default function CardDetails() {
       <NavBar/>
       <div className="bg-white">
         <div className="pt-6">
-           <h1 id="title">Suites de aventura Skylodge</h1>
+           <h1 id='title'>Skylodge Adventure Suites</h1>
 
           {/* Image gallery */}
           <div className="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
@@ -111,7 +111,7 @@ export default function CardDetails() {
           <div className="mx-auto max-w-2xl px-4 pb-16 pt-10 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pb-24 lg:pt-16">
             <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
               <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
-              Lugar para quedarse. Anfitrión: Natalia
+                {product.name}
               </h1>
             </div>
 
@@ -152,15 +152,133 @@ export default function CardDetails() {
 
               <form className="mt-10">
                 {/* Colors */}
-              
+                <div>
+                  <h3 className="text-sm font-medium text-gray-900">Color</h3>
+
+                  <RadioGroup
+                    value={selectedColor}
+                    onChange={setSelectedColor}
+                    className="mt-4"
+                  >
+                    <RadioGroup.Label className="sr-only">
+                      Choose a color
+                    </RadioGroup.Label>
+                    <div className="flex items-center space-x-3">
+                      {product.colors.map((color) => (
+                        <RadioGroup.Option
+                          key={color.name}
+                          value={color}
+                          className={({ active, checked }) =>
+                            classNames(
+                              color.selectedClass,
+                              active && checked ? "ring ring-offset-1" : "",
+                              !active && checked ? "ring-2" : "",
+                              "relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 focus:outline-none"
+                            )
+                          }
+                        >
+                          <RadioGroup.Label as="span" className="sr-only">
+                            {color.name}
+                          </RadioGroup.Label>
+                          <span
+                            aria-hidden="true"
+                            className={classNames(
+                              color.class,
+                              "h-8 w-8 rounded-full border border-black border-opacity-10"
+                            )}
+                          />
+                        </RadioGroup.Option>
+                      ))}
+                    </div>
+                  </RadioGroup>
+                </div>
 
                 {/* Sizes */}
-              
+                <div className="mt-10">
+                  <div className="flex items-center justify-between">
+                    <h3 className="text-sm font-medium text-gray-900">Size</h3>
+                    <a
+                      href="#"
+                      className="text-sm font-medium text-indigo-600 hover:text-indigo-500"
+                    >
+                      Size guide
+                    </a>
+                  </div>
+
+                  <RadioGroup
+                    value={selectedSize}
+                    onChange={setSelectedSize}
+                    className="mt-4"
+                  >
+                    <RadioGroup.Label className="sr-only">
+                      Choose a size
+                    </RadioGroup.Label>
+                    <div className="grid grid-cols-4 gap-4 sm:grid-cols-8 lg:grid-cols-4">
+                      {product.sizes.map((size) => (
+                        <RadioGroup.Option
+                          key={size.name}
+                          value={size}
+                          disabled={!size.inStock}
+                          className={({ active }) =>
+                            classNames(
+                              size.inStock
+                                ? "cursor-pointer bg-white text-gray-900 shadow-sm"
+                                : "cursor-not-allowed bg-gray-50 text-gray-200",
+                              active ? "ring-2 ring-indigo-500" : "",
+                              "group relative flex items-center justify-center rounded-md border py-3 px-4 text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6"
+                            )
+                          }
+                        >
+                          {({ active, checked }) => (
+                            <>
+                              <RadioGroup.Label as="span">
+                                {size.name}
+                              </RadioGroup.Label>
+                              {size.inStock ? (
+                                <span
+                                  className={classNames(
+                                    active ? "border" : "border-2",
+                                    checked
+                                      ? "border-indigo-500"
+                                      : "border-transparent",
+                                    "pointer-events-none absolute -inset-px rounded-md"
+                                  )}
+                                  aria-hidden="true"
+                                />
+                              ) : (
+                                <span
+                                  aria-hidden="true"
+                                  className="pointer-events-none absolute -inset-px rounded-md border-2 border-gray-200"
+                                >
+                                  <svg
+                                    className="absolute inset-0 h-full w-full stroke-2 text-gray-200"
+                                    viewBox="0 0 100 100"
+                                    preserveAspectRatio="none"
+                                    stroke="currentColor"
+                                  >
+                                    <line
+                                      x1={0}
+                                      y1={100}
+                                      x2={100}
+                                      y2={0}
+                                      vectorEffect="non-scaling-stroke"
+                                    />
+                                  </svg>
+                                </span>
+                              )}
+                            </>
+                          )}
+                        </RadioGroup.Option>
+                      ))}
+                    </div>
+                  </RadioGroup>
+                </div>
+
                 <button
                   type="submit"
                   className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                 >
-                 Reservar
+                  Add to bag
                 </button>
               </form>
             </div>
@@ -197,13 +315,7 @@ export default function CardDetails() {
                 <h2 className="text-sm font-medium text-gray-900">Details</h2>
 
                 <div className="mt-4 space-y-6">
-                  <p className="text-sm text-gray-600">Levantándose del suelo como algo sacado de un cuento de hadas, The Boot es uno de esos verdaderos lugares únicos. Este fascinante escondite se encuentra en la costa de Tasmania, una zona de belleza natural y costa escarpada, respaldada por paisajes montañosos para explorar. Su alojamiento es una hazaña creativa de la arquitectura, con grandes chimeneas abiertas por dentro y por fuera, una lujosa cama tamaño queen, una ducha construida para dos, flores, chocolates y golosinas caseras, y su propio patio privado.
-El alojamiento
-El vecino Jester Cafe está bajo la misma propiedad y en la misma propiedad. Aquí puedes disfrutar de la delicia de desayunar en la cafetería, almorzar o pedir una bandeja de cena para disfrutar en The Boot.
-Acceso de los huéspedes
-El Boot está situado en un patio privado en el jardín de nuestra propiedad de 2,4 hectáreas. Usted es libre de pasear por el jardín, caminar por la colina y disfrutar de las vistas desde la cima.
-Otros aspectos para tener en cuenta
-El Boot es un lugar único para hospedarte y, en general, los huéspedes no quieren salir. Si estás buscando algo fuera de lo común, este es tu lugar.</p>
+                  <p className="text-sm text-gray-600">{product.details}</p>
                 </div>
               </div>
             </div>
