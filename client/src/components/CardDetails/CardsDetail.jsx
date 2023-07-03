67fc0a114febeca3a89a80f6509afc85a233e08b
Ariel-Alegre <arielalegre98@gmail.com>
2023-07-03 18:24:29 -0300

    json install

--- client/src/components/CardDetails/CardsDetail.jsx
+++ client/src/components/CardDetails/CardsDetail.jsx
@@ -111,7 +111,7 @@ export default function CardDetails() {
           <div className="mx-auto max-w-2xl px-4 pb-16 pt-10 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pb-24 lg:pt-16">
             <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
               <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
-                {product.name}
+              Lugar para quedarse. Anfitrión: Natalia
               </h1>
             </div>
 
@@ -152,133 +152,16 @@ export default function CardDetails() {
 
               <form className="mt-10">
                 {/* Colors */}
-                <div>
-                  <h3 className="text-sm font-medium text-gray-900">Color</h3>
-
-                  <RadioGroup
-                    value={selectedColor}
-                    onChange={setSelectedColor}
-                    className="mt-4"
-                  >
-                    <RadioGroup.Label className="sr-only">
-                      Choose a color
-                    </RadioGroup.Label>
-                    <div className="flex items-center space-x-3">
-                      {product.colors.map((color) => (
-                        <RadioGroup.Option
-                          key={color.name}
-                          value={color}
-                          className={({ active, checked }) =>
-                            classNames(
-                              color.selectedClass,
-                              active && checked ? "ring ring-offset-1" : "",
-                              !active && checked ? "ring-2" : "",
-                              "relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 focus:outline-none"
-                            )
-                          }
-                        >
-                          <RadioGroup.Label as="span" className="sr-only">
-                            {color.name}
-                          </RadioGroup.Label>
-                          <span
-                            aria-hidden="true"
-                            className={classNames(
-                              color.class,
-                              "h-8 w-8 rounded-full border border-black border-opacity-10"
-                            )}
-                          />
-                        </RadioGroup.Option>
-                      ))}
-                    </div>
-                  </RadioGroup>
-                </div>
+   
 
                 {/* Sizes */}
-                <div className="mt-10">
-                  <div className="flex items-center justify-between">
-                    <h3 className="text-sm font-medium text-gray-900">Size</h3>
-                    <a
-                      href="#"
-                      className="text-sm font-medium text-indigo-600 hover:text-indigo-500"
-                    >
-                      Size guide
-                    </a>
-                  </div>
-
-                  <RadioGroup
-                    value={selectedSize}
-                    onChange={setSelectedSize}
-                    className="mt-4"
-                  >
-                    <RadioGroup.Label className="sr-only">
-                      Choose a size
-                    </RadioGroup.Label>
-                    <div className="grid grid-cols-4 gap-4 sm:grid-cols-8 lg:grid-cols-4">
-                      {product.sizes.map((size) => (
-                        <RadioGroup.Option
-                          key={size.name}
-                          value={size}
-                          disabled={!size.inStock}
-                          className={({ active }) =>
-                            classNames(
-                              size.inStock
-                                ? "cursor-pointer bg-white text-gray-900 shadow-sm"
-                                : "cursor-not-allowed bg-gray-50 text-gray-200",
-                              active ? "ring-2 ring-indigo-500" : "",
-                              "group relative flex items-center justify-center rounded-md border py-3 px-4 text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6"
-                            )
-                          }
-                        >
-                          {({ active, checked }) => (
-                            <>
-                              <RadioGroup.Label as="span">
-                                {size.name}
-                              </RadioGroup.Label>
-                              {size.inStock ? (
-                                <span
-                                  className={classNames(
-                                    active ? "border" : "border-2",
-                                    checked
-                                      ? "border-indigo-500"
-                                      : "border-transparent",
-                                    "pointer-events-none absolute -inset-px rounded-md"
-                                  )}
-                                  aria-hidden="true"
-                                />
-                              ) : (
-                                <span
-                                  aria-hidden="true"
-                                  className="pointer-events-none absolute -inset-px rounded-md border-2 border-gray-200"
-                                >
-                                  <svg
-                                    className="absolute inset-0 h-full w-full stroke-2 text-gray-200"
-                                    viewBox="0 0 100 100"
-                                    preserveAspectRatio="none"
-                                    stroke="currentColor"
-                                  >
-                                    <line
-                                      x1={0}
-                                      y1={100}
-                                      x2={100}
-                                      y2={0}
-                                      vectorEffect="non-scaling-stroke"
-                                    />
-                                  </svg>
-                                </span>
-                              )}
-                            </>
-                          )}
-                        </RadioGroup.Option>
-                      ))}
-                    </div>
-                  </RadioGroup>
-                </div>
+            
 
                 <button
                   type="submit"
                   className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                 >
-                  Add to bag
+                  Reservar
                 </button>
               </form>
             </div>
@@ -290,7 +173,19 @@ export default function CardDetails() {
 
                 <div className="space-y-6">
                   <p className="text-base text-gray-900">
-                    {product.description}
+                  ¿Alguna vez quisiste dormir en el nido de un cóndor? ¡Esto es lo mejor! Una cápsula de lujo transparente que cuelga de la cima de una montaña en el Valle Sagrado del Perú.
+
+Tenemos 3 cápsulas, capacidad máxima en el albergue 12 personas por noche.
+El alojamiento
+¿Alguna vez quisiste dormir en el nido de un cóndor? ¡Esto es lo mejor! Una cápsula de lujo transparente que cuelga de la cima de una montaña en el Valle Sagrado del Perú.
+
+Ubicadas en el Valle Sagrado de Cuzco, Perú, las exclusivas Skylodge Adventure Suites le ofrecen la oportunidad de dormir dentro de un dormitorio colgante completamente transparente, que le permite apreciar la impresionante vista de este mágico y místico valle.
+
+Para dormir en Skylodge, la gente debe subir 400 metros de Via Ferrata o caminar un sendero intrépido a través de tirolinas. Una noche en este lugar hará realidad tus sueños. Los paquetes incluyen desayuno y cena gourmet con vino, transporte desde Cuzco y nuestros guías bilingües profesionales.
+
+Cuota de US$ 450 por persona - 2017
+Acceso de los huéspedes
+Equipamiento, snack, cena gourmet y desayuno
                   </p>
                 </div>
               </div>
@@ -315,7 +210,19 @@ export default function CardDetails() {
                 <h2 className="text-sm font-medium text-gray-900">Details</h2>
 
                 <div className="mt-4 space-y-6">
-                  <p className="text-sm text-gray-600">{product.details}</p>
+                  <p className="text-sm text-gray-600">¿Alguna vez quisiste dormir en el nido de un cóndor? ¡Esto es lo mejor! Una cápsula de lujo transparente que cuelga de la cima de una montaña en el Valle Sagrado del Perú.
+
+Tenemos 3 cápsulas, capacidad máxima en el albergue 12 personas por noche.
+El alojamiento
+¿Alguna vez quisiste dormir en el nido de un cóndor? ¡Esto es lo mejor! Una cápsula de lujo transparente que cuelga de la cima de una montaña en el Valle Sagrado del Perú.
+
+Ubicadas en el Valle Sagrado de Cuzco, Perú, las exclusivas Skylodge Adventure Suites le ofrecen la oportunidad de dormir dentro de un dormitorio colgante completamente transparente, que le permite apreciar la impresionante vista de este mágico y místico valle.
+
+Para dormir en Skylodge, la gente debe subir 400 metros de Via Ferrata o caminar un sendero intrépido a través de tirolinas. Una noche en este lugar hará realidad tus sueños. Los paquetes incluyen desayuno y cena gourmet con vino, transporte desde Cuzco y nuestros guías bilingües profesionales.
+
+Cuota de US$ 450 por persona - 2017
+Acceso de los huéspedes
+Equipamiento, snack, cena gourmet y desayuno</p>
                 </div>
               </div>
             </div>
