b6a1a53c160fdf36739a7a639b76ebc748139341
Ariel-Alegre <arielalegre98@gmail.com>
2023-08-18 16:29:56 -0300

    update

--- client/src/components/FooterHome/FooterHome.jsx
+++ client/src/components/FooterHome/FooterHome.jsx
@@ -1,15 +1,13 @@
-import  './FooterHome.scss'
+import "./FooterHome.scss";
+import { Link } from "react-router-dom";
 
 export default function FooterHome() {
-    return (
-        <div className="footer-container">
-            <div className='text-footer-home'>
-                <span>© 2023 Place Enc</span>
-                {" "}
-                <span>-</span>
-                {" "}
-                <a href="">Términos y Privacidad</a>
-            </div>
-        </div>
-    )
+  return (
+    <div className="footer-container">
+      <div className="text-footer-home">
+        <span>© 2023 Place Enc</span> <span>-</span>{" "}
+        <Link to="/">Términos y Privacidad</Link>
+      </div>
+    </div>
+  );
 }
