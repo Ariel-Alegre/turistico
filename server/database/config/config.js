63220c55c5e92b1bb928c6d8c29332d08e664e5e
Ariel-Alegre <arielalegre98@gmail.com>
2023-09-08 16:48:27 -0300

    update

--- server/database/config/config.js
+++ server/database/config/config.js
@@ -9,7 +9,7 @@ console.log(process.env.PGHOST);
 
 
 module.exports = {
-  development: {
+   development: {
     username:'postgres',
     password: 'PAD6PVvBXV7mcePPdtAJ',
     database: 'railway',
@@ -25,7 +25,25 @@ module.exports = {
     host: 'containers-us-west-41.railway.app',
     port: '5953',
     dialect: "postgres"
+  }, 
+}
+/*   development: {
+    username:'postgres',
+    password: 'password',
+    database: 'turistic',
+    host: 'localhost',
+    port: '5432',
+    dialect: "postgres"
   },
-} 
+
+  production: {
+    username:'postgres',
+    password: 'password',
+    database: 'turistic',
+    host: 'localhost',
+    port: '5432',
+    dialect: "postgres"
+  },
+}  */
 
 
