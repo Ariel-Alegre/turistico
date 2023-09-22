e65d053a7c319fe16fad4c154b875d25daa47e61
Ariel-Alegre <arielalegre98@gmail.com>
2023-09-22 16:58:50 -0300

    update

--- server/database/config/config.js
+++ server/database/config/config.js
@@ -1,38 +1,17 @@
 require('dotenv').config()
-console.log(process.env.PGUSER);
-console.log(process.env.PGPASSWORD);
-console.log(process.env.PGDATABASE);
-console.log(process.env.PGPORT);
-console.log(process.env.PGHOST);
+
 
 
 
 
 module.exports = {
-/*    development: {
-    username:'postgres',
-    password: 'PAD6PVvBXV7mcePPdtAJ',
-    database: 'railway',
-    host: 'containers-us-west-41.railway.app',
-    port: '5953',
-    dialect: "postgres"
-  },
 
-  production: {
-    username:'postgres',
-    password: 'PAD6PVvBXV7mcePPdtAJ',
-    database: 'railway',
-    host: 'containers-us-west-41.railway.app',
-    port: '5953',
-    dialect: "postgres"
-  },   
- */
    development: {
     username: process.env.PGUSER,
   password: process.env.PGPASSWORD,
   database: process.env.PGDATABASE,
   host:process.env.PGHOST,
-  port: process.env.PGHOST,
+  port: process.env.PGPORT,
   dialect: 'postgres'
   },
 
@@ -41,7 +20,7 @@ module.exports = {
     password: process.env.PGPASSWORD,
     database: process.env.PGDATABASE,
     host:process.env.PGHOST,
-    port: process.env.PGHOST,
+    port: process.env.PGPORT,
     dialect: 'postgres'
   }, 
 }  
