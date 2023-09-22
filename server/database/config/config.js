33cbda05e79744d0214b1c7c19b469334f4a3d21
Ariel-Alegre <arielalegre98@gmail.com>
2023-09-22 17:25:26 -0300

    update

--- server/database/config/config.js
+++ server/database/config/config.js
@@ -8,10 +8,13 @@ console.log(process.env.DATABASE_URL);
 module.exports = {
     development: {
       url: 'postgres://elari:Mwi5VpKFvabakGQo6xKXjpkej0Mj3q4Z@dpg-ck6ubmnq54js73ae9esg-a.oregon-postgres.render.com/turistic', // Utiliza la URL proporcionada en tu entorno
+     ssl: true,
       dialect: 'postgres'
     },
     production: {
       url: 'postgres://elari:Mwi5VpKFvabakGQo6xKXjpkej0Mj3q4Z@dpg-ck6ubmnq54js73ae9esg-a.oregon-postgres.render.com/turistic', // Utiliza la URL proporcionada en tu entorno
+      ssl: true,
+    
       dialect: 'postgres'
     }
   // ... otras configuraciones ...
