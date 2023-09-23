54f3d9fb059767cbd8e732100ca0ac316071a4f1
Ariel-Alegre <arielalegre98@gmail.com>
2023-09-23 10:01:48 -0300

    update

--- server/database/config/config.js
+++ server/database/config/config.js
@@ -1,44 +1,34 @@
-require('dotenv').config()
+require('dotenv').config();
 
 console.log(process.env.DATABASE_URL);
-
-
-
 
 module.exports = {
   development: {
     dialect: 'postgres',
-    host: 'dpg-ck6ubmnq54js73ae9esg-a',
+    host: process.env.DB_HOST || 'localhost', // Utiliza la variable de entorno o un valor predeterminado
     port: 5432,
     username: 'elari',
-    password: 'Mwi5VpKFvabakGQo6xKXjpkej0Mj3q4Z', // Deja esto en blanco si no tienes una contraseña
+    password: 'Mwi5VpKFvabakGQo6xKXjpkej0Mj3q4Z',
     database: 'turistic',
-    ssl: true, // Habilita SSL/TLS si es necesario
+    ssl: true,
     dialectOptions: {
       ssl: {
-        require: true, // Esto indica que SSL/TLS es requerido si es necesario
+        require: true,
       },
     },
   },
   production: {
     dialect: 'postgres',
-    host: 'dpg-ck6ubmnq54js73ae9esg-a',
+    host: process.env.DB_HOST || 'localhost', // Utiliza la variable de entorno o un valor predeterminado
     port: 5432,
     username: 'elari',
-    password: 'Mwi5VpKFvabakGQo6xKXjpkej0Mj3q4Z', // Deja esto en blanco si no tienes una contraseña
+    password: 'Mwi5VpKFvabakGQo6xKXjpkej0Mj3q4Z',
     database: 'turistic',
-    ssl: true, // Habilita SSL/TLS si es necesario
+    ssl: true,
     dialectOptions: {
       ssl: {
-        require: true, // Esto indica que SSL/TLS es requerido si es necesario
+        require: true,
       },
     },
   },
-  // Otras configuraciones...
 };
-
-
-
-
-
-
