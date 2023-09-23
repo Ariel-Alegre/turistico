bf8596d9e89f01facc4dd40cd1224ae10a2d9f9c
Ariel-Alegre <arielalegre98@gmail.com>
2023-09-23 10:06:21 -0300

    update

--- server/database/config/config.js
+++ server/database/config/config.js
@@ -1,11 +1,9 @@
 require('dotenv').config();
 
-console.log(process.env.DATABASE_URL);
-
 module.exports = {
   development: {
     dialect: 'postgres',
-    host: process.env.DB_HOST || 'localhost', // Utiliza la variable de entorno o un valor predeterminado
+    host: process.env.DATABASE_URL || 'localhost',
     port: 5432,
     username: 'elari',
     password: 'Mwi5VpKFvabakGQo6xKXjpkej0Mj3q4Z',
@@ -19,7 +17,7 @@ module.exports = {
   },
   production: {
     dialect: 'postgres',
-    host: process.env.DB_HOST || 'localhost', // Utiliza la variable de entorno o un valor predeterminado
+    host: process.env.DATABASE_URL || 'localhost',
     port: 5432,
     username: 'elari',
     password: 'Mwi5VpKFvabakGQo6xKXjpkej0Mj3q4Z',
