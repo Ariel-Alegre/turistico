de25ea29a957a7ba3ebd1d61c0c810452b9753a9
Ariel-Alegre <arielalegre98@gmail.com>
2023-09-24 00:01:34 -0300

    update

--- server/database/config/config.js
+++ server/database/config/config.js
@@ -3,30 +3,20 @@ require('dotenv').config();
 module.exports = {
   development: {
     dialect: 'postgres',
-    host: process.env.DATABASE_URL || 'localhost',
+    host:  'localhost',
     port: 5432,
-    username: 'elari',
-    password: 'Mwi5VpKFvabakGQo6xKXjpkej0Mj3q4Z',
+    username: 'postgres',
+    password: 'password',
     database: 'turistic',
-    ssl: true,
-    dialectOptions: {
-      ssl: {
-        require: true,
-      },
-    },
+
   },
   production: {
     dialect: 'postgres',
-    host: process.env.DATABASE_URL || 'localhost',
+    host:  'localhost',
     port: 5432,
-    username: 'elari',
-    password: 'Mwi5VpKFvabakGQo6xKXjpkej0Mj3q4Z',
+    username: 'postgres',
+    password: 'password',
     database: 'turistic',
-    ssl: true,
-    dialectOptions: {
-      ssl: {
-        require: true,
-      },
-    },
+   
   },
 };
